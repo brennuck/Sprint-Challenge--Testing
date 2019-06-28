e9f32e384478b9ba728c3aa445237370667ff0a1
Brennon Nuckols <brennuck@gmail.com>
2019-06-28 10:30:34 -0600

    GET/games done

--- app.spec.js
+++ app.spec.js
@@ -11,13 +11,30 @@ describe("server", () => {
     });
     it("should return JSON", async () => {
       const res = await request(server).get("/");
-      
+
       expect(res.type).toBe("application/json");
     });
     it('should return { message: "Server g2g" }', async () => {
       const res = await request(server).get("/");
 
       expect(res.body).toEqual({ message: "Server g2g" });
+    });
+  });
+  describe("GET /games", () => {
+    it("returns the array of games", async () => {
+      const res = await request(server).get("/games");
+
+      expect(res.type).toBe("application/json");
+    });
+    it("returns status code 200", async () => {
+      const res = await request(server).get("/games");
+
+      expect(res.status).toBe(200);
+    });
+    it("returns an array", async () => {
+      const res = await request(server).get("/games");
+
+      expect(Array.isArray(res.body)).toBe(true);
     });
   });
 });
